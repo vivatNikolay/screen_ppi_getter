fe90380a5c5b9e439dc42f261c023ba7fc7cb49d
gasaichandesu <ourea121@gmail.com>
2024-12-20 03:06:52 +0400

    chore: update dependencies, upgrade example project, fix invalid resources path when trying to get ios devices list

--- lib/src/resources/assets.dart
+++
@@ -1,7 +0,0 @@
-part of 'resources.dart';
-
-class Assets {
-  Assets._();
-
-  static const String devices = 'generated/devices.json';
-}
--- lib/src/resources/resources.dart
+++ lib/src/resources/resources.dart
@@ -1,1 +1,4 @@
-part 'assets.dart';
+/// Constants for resources path
+abstract class Resources {
+  static const iosDevices = 'packages/screen_ppi_getter/generated/devices.json';
+}
